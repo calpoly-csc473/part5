9c682a6163e8b60404f987f12850ff9d2918c705
Ian Dunn <iondune@gmail.com>
2018-05-15 18:45:44 -0700

    Fix missing glm namespace qualifier

--- src/Shading/BlinnPhongBRDF.cpp
+++ src/Shading/BlinnPhongBRDF.cpp
@@ -13,7 +13,7 @@ glm::vec3 BlinnPhongBRDF::CalculateDiffuse(const Material & Material, const Surf
 
 glm::vec3 BlinnPhongBRDF::CalculateSpecular(const Material & Material, const SurfaceVectors & surface) const
 {
-	glm::vec3 const half = normalize(surface.light + surface.view);
+	glm::vec3 const half = glm::normalize(surface.light + surface.view);
 	float const power = (2.f / (Material.finish.roughness * Material.finish.roughness) - 2.f);
 
 	return glm::vec3(glm::pow(glm::clamp(glm::dot(surface.normal, half), 0.f, 1.f), power));
