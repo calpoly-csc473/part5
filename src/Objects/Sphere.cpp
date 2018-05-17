d0f60f3ef17fbad626fcfe775a1f7e349159aefc
Ian Dunn <iondune@gmail.com>
2018-05-17 00:43:57 -0700

    Remove optimizations from sphere intersect so it matches lecture version

--- src/Objects/Sphere.cpp
+++ src/Objects/Sphere.cpp
@@ -13,30 +13,25 @@ Sphere::Sphere(const glm::vec3 & c, const float r)
 float Sphere::Intersect(const Ray & ray) const
 {
 	const glm::vec3 offset = ray.origin - center;
+
 	const float a = glm::dot(ray.direction, ray.direction);
-	const float b = glm::dot(ray.direction, offset);
+	const float b = 2.f * glm::dot(ray.direction, offset);
 	const float c = glm::dot(offset, offset) - radius*radius;
 
-	const float discriminant = b * b - a * c;
+	const float discriminant = b * b - 4.f * a * c;
 
 	if (discriminant >= 0.f)
 	{
-		float dividend;
-		if (b < 0)
+		const float sqrtDiscriminant = sqrt(discriminant);
+
+		float numerator = -b - sqrtDiscriminant;
+
+		if (numerator < 0)
 		{
-			dividend = -b + sqrt(discriminant);
-		}
-		else
-		{
-			dividend = -b - sqrt(discriminant);
+			numerator = -b + sqrtDiscriminant;
 		}
 
-		float t = c / dividend;
-
-		if (t <= 0.f)
-		{
-			t = dividend / a;
-		}
+		const float t = numerator / (2.f * a);
 
 		if (t >= 0.f)
 		{
